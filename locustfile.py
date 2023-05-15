22b5f0f5c78558da8126ecfe607423220d30bb56
kimurarinntaro <kimura_rintaro@mail.toyota.co.jp>
2023-05-15 18:12:46 +0900

    pythonFileの編集

--- locustfile.py
+++ locustfile.py
@@ -1,13 +1,46 @@
 from locust import HttpUser, TaskSet, task, between, constant
+import itertools
+
+XLSX_MIMETYPE = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+# filePath = "/Users/rintarokimura/Desktop/neko.xlsx"
+# fileDataBinary = open(filePath, "rb")
+# data = {"uploadFile": (filePath, fileDataBinary, XLSX_MIMETYPE)}
 
 class UserBehavior(TaskSet):
+    test_file = None
+
+    def on_start(self):
+        self.test_file = itertools.cycle(["/Users/rintarokimura/Desktop/neko.xlsx"])
+
+# @task(1)
+    # def search(self):
+    #     self.client.get("/boards/KT662", verify=False)
+
+    # @task(2)
+    # def search(self):
+    #     response = self.client.post(
+    #         url="/api/kumi/KTZZZ/files",
+    #         files=data,
+    #     )
+    #     print(response.text)
+
+    # @task(1)
+    # def search(self):
+    #     self.client.get("/", verify=False)
 
     @task(1)
     def search(self):
-        self.client.get("/", verify=False)
+        url = "/api/kumi/KT662/files"
+        file_path = next(self.test_file)
+        # headers = {
+        #     "Content-Type": "multipart/form-data; boundary=----WebKitFormBoundary7MA4YWxkTrZu0gW"
+        # }
+        with open(file_path, "rb") as f:
+            files = {"file": f}
+            response = self.client.post(url, files=files)
+            print(response.status_code)
 
 class WebsiteUser(HttpUser):
-
-    tasks = {UserBehavior:1}
     wait_time = constant(0)
+    tasks = [UserBehavior]
     
